e8b6b1ac17b874f0106f09df3d47db4dcd79760c
AuliyaIkhsanaNasution <auliyanasti06@gmail.com>
2025-01-24 15:30:36 +0700

    update : logo in pdf.php

--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -4,6 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('img/logo.png') }}">
+    <link rel="icon" type="image/png" href="{{ asset('img/logo.png') }}">
     <title>{{ $title }}</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
